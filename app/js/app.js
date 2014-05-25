8edcc4027f34cd453bb9c7c9fc37931a926c3163
Andrew Wittrock <awittrock@gmail.com>
2014-05-25 12:42:57 -0500

    adding routing to check out driver details

--- app/js/app.js
+++ app/js/app.js
@@ -23,7 +23,7 @@ angular.module('F1FeederApp', [
   'F1FeederApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/drivers', {templateUrl: 'partials/drivers.html', controller: 'driversController'});
+  $routeProvider.when('/drivers/:id', {templateUrl: 'partials/driver.html', controller: 'driversController'});
+  $routeProvider.otherwise({redirectTo: '/drivers'});
 }]);
