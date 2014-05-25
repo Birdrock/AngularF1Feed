3a497625bd3ee4fb712fe353b19da639cec4dbb2
Andrew Wittrock <awittrock@gmail.com>
2014-05-25 12:29:10 -0500

    added search filters; two way data binding with ng-model

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -39,6 +39,11 @@ angular.module('F1FeederApp.controllers', []).
 	$scope.nameFilter = null;
 	$scope.driversList = [];
 
+	$scope.searchFilter = function (driver) {
+		var keyword = new RegExp($scope.nameFilter, 'i');
+		return !$scope.nameFilter || keyword.test(driver.Driver.givenName) || keyword.test(driver.Driver.familyName);
+	};
+
 	ergastAPIservice.getDrivers().success(function (response) {
 		$scope.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
 	});
